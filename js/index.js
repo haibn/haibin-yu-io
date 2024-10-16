5a3adf8b27a4c8bc264a75f4df774304acdf68d6
haibn <haibinyuy@berkeley.edu>
2024-10-15 23:23:26 -0700

    revisions corrected

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
+/*
+
+  Footer
+
+*/
+
+// Creates and appends a footer element at the end of the body
 let body = document.querySelector("body");
 let newFooter = document.createElement("footer");
 body.appendChild(newFooter);
 
+// Appends the current year and copyright logo to the footer element created above
 let today = new Date();
 let thisYear = today.getFullYear();
 let footer = document.querySelector("footer");
@@ -9,6 +17,13 @@ let copyright = document.createElement("p");
 copyright.textContent = `Haibin Yu ${thisYear} \u00A9`;
 footer.appendChild(copyright);
 
+/*
+
+  Skills
+
+*/
+
+// Creates and appends a li element to the skills list for each element in the skills array 
 let skills = ["HTML", "CSS", "JS", "Python", "Java", "C", "SQL", "Github", "GIMP", "Photoshop", "HitFilm", "CapCut"];
 let skillsSection = document.querySelector("#skills");
 let skillsList = skillsSection.querySelector("ul");
@@ -18,16 +33,19 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
+/*
+
+  Message form
+
+*/
+
 let messageForm = document.getElementsByName('leave_message')[0];
 
+// Create a function such that when the submit button is clicked, it creates a new li element and displays it in the Message section
 function submitBtn(event) {
     event.preventDefault();
-    let formName = messageForm.querySelector('input[name="usersName"]').value;
     let formEmail = messageForm.querySelector('input[name="usersEmail"]').value;
     let formMessage = messageForm.querySelector('textarea[name="usersMessage"]').value;
-    console.log(formName);
-    console.log(formEmail);
-    console.log(formMessage);
     event.target.reset();
     let messageSection = document.querySelector("#messages");
     let messageList = messageSection.querySelector("ul");
@@ -46,6 +64,13 @@ function submitBtn(event) {
 }
 messageForm.addEventListener("submit", submitBtn);
 
+/*
+
+  Projects
+
+*/
+
+// Access githubs api to fetch and display my projects in the project section
 let projectSection = document.querySelector("#projects");
 let projectList = projectSection.querySelector("ul");
 
